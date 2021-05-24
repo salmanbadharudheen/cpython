2d9eeab7a39879841a49a7bcac1d6edd8d0234bd
Salmanul faris <84730854+salmanbadharudheen@users.noreply.github.com>
2021-05-24 22:50:59 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,13 @@
+def largest_number(list):
+             numbers=int(input("enter a number")
+             large_no=list[ ]
+             for a in list:
+                if a>large_no:
+                   large_no=a
+          return large_no
+print (largest_number)
+
+    
+
+
+
