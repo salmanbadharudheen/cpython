1d1b2699da05cf8176f0d18e40226b88a7118504
Salmanul faris <84730854+salmanbadharudheen@users.noreply.github.com>
2021-05-24 22:53:13 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,17 @@
+def first_twp(list):
+        numbers=int(input("enter a number")
+        large_no=list[ ]
+        for a in list:
+             if a>large_no:
+                  large_no=a
+        return large_no
+        second_large=[[a],[large_no]]
+        for s in list:
+             if s>second_large:
+                  second_large=s
+         return second_large
+
+    
+
+
+
