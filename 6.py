a01407d9764035031d79cd3a06fa69b7aa5d6bd7
Salmanul faris <84730854+salmanbadharudheen@users.noreply.github.com>
2021-05-24 22:55:11 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,10 @@
+import string
+               def names():
+                    alphabet=set(string.asci_lowercase)
+                     input_string=" The quick brown fox jumps over the lazy dog"
+     print(set(input_string.lower())>=alphabet)
+
+    
+
+
+
